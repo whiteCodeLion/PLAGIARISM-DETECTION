2949bd658913295d5f584cebbf46dfddacc85508
White code Lion <122578876+whiteCodeLion@users.noreply.github.com>
2023-02-15 10:47:06 +0100

    Update home.js

--- home.js
+++ home.js
@@ -1,20 +1,23 @@
-$(document).ready(function() {
-    $('form').submit(function(event) {
-        event.preventDefault();
-        var formData = new FormData($('form')[0]);
+        const form = document.querySelector('#upload-form');
+        const resultDiv = document.querySelector('#result');
 
-        $.ajax({
-            url: '/upload',
-            type: 'POST',
-            data: formData,
-            processData: false,
-            contentType: false,
-            success: function(response) {
-                $('#result').text(response.result);
-            },
-            error: function(error) {
-                console.log(error);
-            }
+        form.addEventListener('submit', event => {
+            event.preventDefault();
+            const formData = new FormData(form);
+            fetch('/upload', {
+                method: 'POST',
+                body: formData,
+            })
+                .then(response => response.json())
+                .then(data => {
+                    const plagiarismDetected = data.plagiarism_detected;
+                    if (plagiarismDetected) {
+                        resultDiv.innerHTML = 'Plagiarism detected! The similarity score is ' + data.similarity.toFixed(2);
+                    } else {
+                        resultDiv.innerHTML = 'No plagiarism detected!';
+                    }
+                })
+                .catch(error => {
+                    resultDiv.innerHTML = 'An error occurred: ' + error.message;
+                });
         });
-    });
-});
